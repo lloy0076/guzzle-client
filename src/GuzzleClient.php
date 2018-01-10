782c373b9fb3ff685c1b39ec46a04b06a89f71fd
David Lloyd <lloy0076@adam.com.au>
2018-01-10 06:03:44 -0500

    Add methods for get, delete, head, options, patch, post and put.
    See: http://docs.guzzlephp.org/en/stable/quickstart.html
    
    Note that Guzzle uses magic methods to implement this, see [Line 78](https://github.com/guzzle/guzzle/blob/master/src/Client.php#L78).
    
    This uses the actual names as magic methods strikes @lloy0076 as too obscure.

--- src/GuzzleClient.php
+++ src/GuzzleClient.php
@@ -33,6 +33,7 @@ class GuzzleClient implements RequestClientContract
 
     /**
      * GuzzleClient constructor.
+     *
      * @param ClientInterface $client
      */
     public function __construct(ClientInterface $client)
@@ -41,13 +42,123 @@ class GuzzleClient implements RequestClientContract
     }
 
     /**
+     * @param string     $uri
+     * @param array|null $body
+     * @param array|null $headers
+     * @param array|null $options
+     * @return \Dpc\GuzzleClient\RequestClientContract
+     */
+    public function get(string $uri,
+                        ?array $body = null,
+                        ?array $headers = null,
+                        ?array $options = null): RequestClientContract
+    {
+        return $this->send('GET', $uri, $body, $headers, $options);
+    }
+
+    /**
      * @inheritDoc
      */
-    public function send(string $method, string $uri, ?array $body = null, ?array $headers = null, ?array $options = null): RequestClientContract
+    public function send(string $method,
+                         string $uri,
+                         ?array $body = null,
+                         ?array $headers = null,
+                         ?array $options = null): RequestClientContract
     {
-        [$this->method, $this->uri, $this->body, $this->headers, $this->options] = [$method, $uri, $body, $headers, $options];
+        [$this->method, $this->uri, $this->body, $this->headers, $this->options] =
+            [$method, $uri, $body, $headers, $options];
 
         return $this;
+    }
+
+    /**
+     * @param string     $uri
+     * @param array|null $body
+     * @param array|null $headers
+     * @param array|null $options
+     * @return \Dpc\GuzzleClient\RequestClientContract
+     */
+    public function delete(string $uri,
+                           ?array $body = null,
+                           ?array $headers = null,
+                           ?array $options = null): RequestClientContract
+    {
+        return $this->send('DELETE', $uri, $body, $headers, $options);
+    }
+
+    /**
+     * @param string     $uri
+     * @param array|null $body
+     * @param array|null $headers
+     * @param array|null $options
+     * @return \Dpc\GuzzleClient\RequestClientContract
+     */
+    public function head(string $uri,
+                         ?array $body = null,
+                         ?array $headers = null,
+                         ?array $options = null): RequestClientContract
+    {
+        return $this->send('HEAD', $uri, $body, $headers, $options);
+    }
+
+    /**
+     * @param string     $uri
+     * @param array|null $body
+     * @param array|null $headers
+     * @param array|null $options
+     * @return \Dpc\GuzzleClient\RequestClientContract
+     */
+    public function options(string $uri,
+                            ?array $body = null,
+                            ?array $headers = null,
+                            ?array $options = null): RequestClientContract
+    {
+        return $this->send('OPTIONS', $uri, $body, $headers, $options);
+    }
+
+    /**
+     * @param string     $uri
+     * @param array|null $body
+     * @param array|null $headers
+     * @param array|null $options
+     * @return \Dpc\GuzzleClient\RequestClientContract
+     */
+    public function patch(string $uri,
+                          ?array $body = null,
+                          ?array $headers = null,
+                          ?array $options = null): RequestClientContract
+    {
+        return $this->send('PATCH', $uri, $body, $headers, $options);
+    }
+
+    /**
+     * @param string     $uri
+     * @param array|null $body
+     * @param array|null $headers
+     * @param array|null $options
+     * @return \Dpc\GuzzleClient\RequestClientContract
+     */
+    public function post(string $uri,
+                         ?array $body = null,
+                         ?array $headers = null,
+                         ?array $options = null): RequestClientContract
+    {
+        return $this->send('POST', $uri, $body, $headers, $options);
+    }
+
+    /**
+     * @param string     $uri
+     * @param array|null $body
+     * @param array|null $headers
+     * @param array|null $options
+     * @return \Dpc\GuzzleClient\RequestClientContract
+     */
+    public function put(string $uri,
+                        ?array $body = null,
+                        ?array $headers = null,
+                        ?array $options = null): RequestClientContract
+    {
+        return $this->send('PUT', $uri, $body, $headers, $options);
     }
 
     /**
@@ -56,6 +167,7 @@ class GuzzleClient implements RequestClientContract
     public function asFormParams(): RequestClientContract
     {
         $this->format = 'form_params';
+
         return $this;
     }
 
@@ -65,6 +177,7 @@ class GuzzleClient implements RequestClientContract
     public function asJson(): RequestClientContract
     {
         $this->format = 'json';
+
         return $this;
 
     }
@@ -78,11 +191,23 @@ class GuzzleClient implements RequestClientContract
     }
 
     /**
-     * @inheritDoc
+     * @return string
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function json()
+    protected function sendRequest(): string
     {
-        return json_decode($this->sendRequest());
+        $response = (string)$this->client->request($this->method,
+            $this->uri,
+            [
+                $this->format => $this->body,
+                'headers'     => $this->headers,
+                'options'     => $this->options,
+                'debug'       => $this->debug,
+            ])->getBody();
+
+        $this->debug = false;
+
+        return $response;
     }
 
     /**
@@ -96,20 +221,10 @@ class GuzzleClient implements RequestClientContract
     }
 
     /**
-     * @return string
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @inheritDoc
      */
-    protected function sendRequest(): string
+    public function json()
     {
-        $response = (string)$this->client->request($this->method, $this->uri, [
-            $this->format => $this->body,
-            'headers' => $this->headers,
-            'options' => $this->options,
-            'debug' => $this->debug
-        ])->getBody();
-
-        $this->debug = false;
-
-        return $response;
+        return json_decode($this->sendRequest());
     }
 }
